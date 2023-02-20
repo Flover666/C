6ea1d6914b9981f3a4d895b8b71ec5d9c189afd0
Flover666 <3405162048@qq.com>
2023-02-20 10:10:01 +0800

    master

---
+++ 1/2.ReverseNum.cpp
@@ -0,0 +1,30 @@
+//mingw的gdb本身不支持中文路径名造成的
+#include <cstdio>
+int Reverse(int n)
+{
+    int reverse = 0;
+    int remain; // 余数
+    while (true)
+    {
+        remain = n % 10;
+        reverse = reverse * 10 + remain;
+        n = n / 10;
+        if (0 == n)
+        {
+            break;
+        }
+    }
+    return reverse;
+}
+
+int main()
+{
+    int i = 0;
+    for (i = 0; i <= 256; ++i)
+    {
+        if (i * i == Reverse(i * i))
+        {
+            printf("%d\n", i);
+        }
+    }
+}
